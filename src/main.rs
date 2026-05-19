db0cbba83bd9aa022bda3eb1ebc01ec92f523739
André Ribeiro <andresribeiroo@proton.me>
2026-05-19 19:10:13 -0300

    basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Json, Router};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Status {
+    ok: bool,
+}
+
+async fn status() -> Json<Status> {
+    Json(Status { ok: true })
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/status", get(status));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
